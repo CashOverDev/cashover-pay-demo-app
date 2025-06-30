d5463b80c70778542deabe2675ecce2c64d59ab0
AnasNaouchi <anas.n@opn.ooo>
2025-06-30 03:07:10 +0300

    Removed unused url

--- lib/createOrder.dart
+++ lib/createOrder.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
-import 'dart:io';
 import 'package:url_launcher/url_launcher.dart';
 
 class CreateOrder extends StatefulWidget {
